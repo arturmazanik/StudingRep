40a22f1ccc4e9c92b2af094b1c0fd165c755947f
arturmazanik <artur.mazanik@ya.ru>
2017-10-09 21:22:40 +0300

    Create project, add a splash screen

---
+++ RwTickests/RwTickests/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace RwTickests
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            Thread t = new Thread(new ThreadStart(SplashScreen));
+            t.Start();
+            Thread.Sleep(2500);
+            InitializeComponent();
+            t.Abort();
+        }
+
+        public void SplashScreen()
+        {
+            Application.Run(new LoadFrm());
+        }
+    }
+}
---
+++ RwTickests/RwTickests/LoadFrm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RwTickests
+{
+    public partial class LoadFrm : Form
+    {
+        public LoadFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Increment(15);
+            if (progressBar1.Value == 100) timer1.Stop();
+        }
+    }
+}
