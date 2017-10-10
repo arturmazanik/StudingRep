2a7b7a17dd852a6b6bd27e57e56a3b337842ac5e
arturmazanik <artur.mazanik@ya.ru>
2017-10-10 16:45:53 +0300

    Add Model and Controller "Product(controller)"

---
+++ BookStore/BookStore/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookStore.Models;
+
+namespace BookStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            List<Product> products = new List<Product>();
+
+            products.Add(new Product()
+            {
+                AuthorName = "Н.В. Гоголь",
+                BookName = "Мёртвые души",
+                About = "«...Говоря о Мертвых душах - можно вдоволь наговориться о России...» -это суждение Вяземского объясняло главную причину споров и определяло особое место книги в истории русской литературы",
+                Price = 15.5
+            });
+
+            return View();
+        }
+    }
+}
---
+++ BookStore/BookStore/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Models
+{
+    public class Product
+    {
+        public int IDictionary { get; set; }
+        public string AuthorName { get; set; }
+        public string BookName { get; set; }
+        public string About { get; set; }
+        public double Price { get; set; }
+    }
+}
